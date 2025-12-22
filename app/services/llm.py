88f4a3f95a7ad26d0d0f26b7f3922d84c4626c81
Garvit Joshi <garvitjoshi@Garvits-MacBook-Pro.local>
2025-12-22 10:35:05 +0530

    Added LLM url

--- app/services/llm.py
+++ app/services/llm.py
@@ -1,2 +1,16 @@
+import requests
+
+LLM_URL = "http://localhost:11434/api/generate"
+
 def generate_response(prompt: str) -> str:
-    return f"LLM response for: {prompt}"
+    payload = {
+        "model": "gemma:2b",
+        "prompt": prompt,
+        "stream": False
+    }
+
+    response = requests.post(LLM_URL, json=payload)
+    data = response.json()
+    return data["response"]
+
+# print(generate_response("Why is the sky blue?"))
